8ac29cd0f67ee07ed6feb3a84386783d1287b870
azadehmn <38600349+azadehmn@users.noreply.github.com>
2018-09-22 16:46:07 +0330

    Update app.js

--- src/app.js
+++ src/app.js
@@ -46,7 +46,91 @@ export default {
 
     let schema = this.schema;
     if (config.schema) {
+      schema = '/' + config.schema;import Vue from 'vue';
+import axios from 'axios';
+
+export default {
+  resources: {},
+  prefix: '/graphql',
+  schema: '', //default schema
+  axios,
+
+  /**
+   *
+   * @param Vue
+   * @param options
+   */
+  install(Vue, options) {
+    Vue.prototype.$graphql = this;
+  },
+
+  /**
+   *
+   * @param resources
+   */
+  setResources(resources) {
+    this.recources = resources;
+  },
+
+  /**
+   *
+   * @param prefix
+   */
+  setPrefix(prefix) {
+    this.prefix = prefix;
+  },
+
+  /**
+   *
+   * @param name
+   * @param config
+   * @returns {AxiosPromise<any>}
+   */
+  query(name = null, config = {query: null, variables: null, schema: ""}) {
+    let resource = config.query;
+    if (!config.query) {
+      resource = this.recources.query[name];
+    }
+
+    let schema = this.schema;
+    if (config.schema) {
       schema = '/' + config.schema;
+    }
+
+    return this.axios.post(
+      this.prefix + schema,
+      {
+        query: resource.replace(/\n|\t/g, ' '),
+        variables: config.variables
+      });
+  },
+
+  /**
+   *
+   * @param name
+   * @param config
+   * @returns {AxiosPromise<any>}
+   */
+  mutation(name = null, config = {query: null, variables: null, schema: ""},options={}) {
+    let resource = config.query;
+    if (!config.query) {
+      resource = this.recources.mutation[name];
+    }
+
+    let schema = '';
+    if (config.schema) {
+      schema = '/' + config.schema;
+    }
+
+    return this.axios.post(
+      this.prefix + schema,
+      {
+        query: resource.replace(/\s+/g, ' '),
+        variables: config.variables
+      },options);
+  },
+}
+
     }
 
     return this.axios.post(
